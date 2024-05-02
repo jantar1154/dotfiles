aab0de83c75961c03b7faa0a23570432e0258879
jantar1154 <davidbednarek@email.cz>
2024-05-02 20:55:55 +0200

    Nvim improvements

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,12 +2,12 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.opt.number = true -- line numbers
+vim.opt.termguicolors = true
 vim.opt.colorcolumn = '80'
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
 vim.opt.autowrite = true
-vim.opt.cursorline = true
 vim.opt.autoread = true
 
 -- Tabs to spaces
@@ -21,15 +21,15 @@ vim.cmd('filetype plugin indent on')
 
 -- NERD Tree
 vim.keymap.set('n', '<F1>', ':NERDTreeToggle<CR>')
+vim.keymap.set('n', '<C-p>', ':NERDTreeToggle<CR>')
 
 -- tabs
-vim.keymap.set('n', '<A-Up>', ':tabnew<CR>')
-vim.keymap.set('n', '<A-Down>', ':tabclose<CR>')
-vim.keymap.set('n', '<A-Left>', ':tabprevious<CR>')
-vim.keymap.set('n', '<A-Right>', ':tabnext<CR>')
+vim.keymap.set('n', '<A-k>', ':tabnew<CR>')
+vim.keymap.set('n', '<A-j>', ':tabclose<CR>')
+vim.keymap.set('n', '<A-h>', ':tabprevious<CR>')
+vim.keymap.set('n', '<A-l>', ':tabnext<CR>')
 vim.keymap.set('n', '<A-S-Left>', ':-tabmove<CR>')
 vim.keymap.set('n', '<A-S-Right>', ':+tabmove<CR>')
-vim.keymap.set("i", "<C-Space>", vim.fn['coc#refresh'])
 
 -- Coc
 vim.api.nvim_set_keymap("i", "<TAB>", "coc#pum#visible() ? coc#_select_confirm(): '<TAB>'", {noremap = true, silent = true, expr = true})
@@ -47,4 +47,10 @@ Plug('andweeb/presence.nvim') -- Discord rich presence
 Plug('mg979/vim-visual-multi')
 Plug('windwp/nvim-autopairs') -- Automatic closing brackets
 
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-tree/nvim-web-devicons')
+Plug('willothy/nvim-cokeline') -- cock line
+
 vim.call('plug#end')
+
+require('cokeline').setup()
